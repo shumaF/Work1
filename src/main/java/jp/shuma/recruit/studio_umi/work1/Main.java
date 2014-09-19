616a814dd181e8cc97da81c8f014b3fed4d6c8b3
shumaF <shuma_fukuda@nike.eonet.ne.jp>
2014-09-20 00:28:18 +0900

    first commit

---
+++ src/main/java/jp/shuma/recruit/studio_umi/work1/Main.java
@@ -0,0 +1,13 @@
+package jp.shuma.recruit.studio_umi.work1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
